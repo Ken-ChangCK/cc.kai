a3ebcd4df993de916e526f12c3ba95e04a86626c
chekai7 <rockkcor75@gmail.com>
2020-10-04 20:45:57 +0800

    Updates

---
+++ precache-manifest.e9df83b4c078c35653205dc350115ec2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf3a95124d62c38c90bb39c64b08eac",
+    "url": "/cc.kai/index.html"
+  },
+  {
+    "revision": "7b7be858bef538d2e93f",
+    "url": "/cc.kai/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3cd3ef2b9ac4fce6f349",
+    "url": "/cc.kai/static/js/2.fcc5dea9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cc.kai/static/js/2.fcc5dea9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b7be858bef538d2e93f",
+    "url": "/cc.kai/static/js/main.d5de7a77.chunk.js"
+  },
+  {
+    "revision": "9503a69e21b8a348ec43",
+    "url": "/cc.kai/static/js/runtime-main.50917677.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cc.kai/static/media/logo.5d5d9eef.svg"
+  }
+]);
